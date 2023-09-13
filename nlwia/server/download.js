32b1eb17c39e3c6b88f9c16a873602921315dbc1
lucas-lucena <lucas.lucena@ccc.ufcg.edu.br>
2023-09-13 12:30:50 -0300

    Fazendo o download do shorts

---
+++ nlwia/server/download.js
@@ -0,0 +1,26 @@
+import ytdl from "ytdl-core"
+import fs from "fs"
+
+export const download = (videoId) => {
+  console.log("Dowloading video:", videoId)
+  const videoURL = "https://www.youtube.com/shorts/" + videoId
+
+  ytdl(videoURL, { quality: "lowestaudio", filter: "audioonly" })
+    .on("info", (info) => {
+      const seconds = info.formats[0].approxDurationMs / 1000
+      console.log(seconds)
+
+      if (seconds > 60) {
+        throw new Error("A duração desse video é maior do que 60 segundos.")
+      }
+    })
+    .on("end", () => {
+      console.log("Download do video foi finalizada.")
+    })
+    .on("error", (Error) => {
+      console.log(
+        "Não foi possível fazer o download do video. Detalhes do erro:",
+        error
+      )
+    }).pipe(fs.createWriteStream('./tmp/audio.mp4'))
+}
--- nlwia/server/index.js
+++ nlwia/server/index.js
@@ -1,10 +1,13 @@
 import cors from 'cors'
 import express from 'express'
 
+import { download } from './download.js'
+
 const app = express()
 app.use(cors())
 
 app.get('/summary/:id', (request, response) => {
+  download(request.params.id)
   response.send('Id do video:' + request.params.id)
 } )
 
