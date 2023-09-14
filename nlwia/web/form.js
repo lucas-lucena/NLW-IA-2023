ba815ace7d726b3873582e5e5251f293729259fa
lucas-lucena <lucas.lucena@ccc.ufcg.edu.br>
2023-09-14 11:02:02 -0300

    Conectando back com o front

--- nlwia/web/form.js
+++ nlwia/web/form.js
@@ -1,10 +1,23 @@
+import {server} from "./server.js"
+
 const form = document.querySelector("#form")
 const input = document.querySelector("#url")
-
+const content = document.querySelector("#content")
 
 form.addEventListener("submit", (event) => {
   event.preventDefault()
 
   const videoURL = input.value
-  console.log("URL do Video:", videoURL)
+
+  if (!videoURL.includes("shorts")) {
+    return (content.textContent = "Esse video não parece ser um short.")
+  }
+
+  const [_, params] = videoURL.split("/shorts/")
+  const [videoID] = params.split("?si")
+  
+  content.textContent = "Obtendo o texto do audio..."
+  server.get("/summary/" + videoID)
+
+  content.textContent = "Realizando o resumo..."
 })
