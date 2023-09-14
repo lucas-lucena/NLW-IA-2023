13f666b15937c8c7b2901fe611abf8f4ad88ae00
lucas-lucena <lucas.lucena@ccc.ufcg.edu.br>
2023-09-14 11:30:31 -0300

    summarize mock e app.post em index.js

--- nlwia/server/index.js
+++ nlwia/server/index.js
@@ -3,16 +3,20 @@ import express from "express"
 
 import { download } from "./download.js"
 import { transcribe } from "./transcribe.js"
+import { summarize } from "./summarize.js"
 
 const app = express()
 app.use(cors())
 
 app.get("/summary/:id", async (request, response) => {
   await download(request.params.id) //du7oVbcWdJs
-  // response.send('Id do video:' + request.params.id)
-  const result =  transcribe()
-
+  const result =  await transcribe()
+  
   response.json({ result })
+})
+
+app.post("/summary", async (request, response) => {
+  await summarize(request.body.text)
 })
 
 app.listen(3333, () => console.log("Server is running on port 3333"))
